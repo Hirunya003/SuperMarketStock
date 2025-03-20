3c6d2614df2ebd88c404049aa523b51c78d72f9d
Indura <inpitadeniya@gmail.com>
2025-03-21 00:28:45 +0530

    fix errors

--- backend/src/main/java/com/SmartMart/backend/BackendApplication.java
+++ backend/src/main/java/com/SmartMart/backend/BackendApplication.java
@@ -2,15 +2,12 @@ package com.SmartMart.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.SmartMart.backend.Supplier.repository")
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-
-
 }
--- backend/src/main/java/com/SmartMart/backend/Supplier/SupplierMongoConfig.java
+++ backend/src/main/java/com/SmartMart/backend/Supplier/SupplierMongoConfig.java
@@ -1,6 +1,5 @@
 package com.SmartMart.backend.Supplier;
 
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
@@ -8,17 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-public class MongoConfig {
-
+public class SupplierMongoConfig {
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://hirunyadimanthi:123@cluster0.e6zqq.mongodb.net/smartmart");
-
+        return MongoClients.create("mongodb+srv://hirunyadimanthi:123@cluster0.e6zqq.mongodb.net/SmartMart");
     }
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "<cluster0.e6zqq.mongodb.net/smartmart>");
+        return new MongoTemplate(mongoClient(), "SmartMart");
     }
-
 }
