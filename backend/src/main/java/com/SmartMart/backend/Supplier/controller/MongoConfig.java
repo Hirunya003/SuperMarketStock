66b2861d38d1625710143ba0f307caba99edd576
indura2003 <inpitadeniya@gmail.com>
2025-03-20 21:20:25 +0530

    pw

--- backend/src/main/java/com/SmartMart/backend/Supplier/controller/MongoConfig.java
+++ backend/src/main/java/com/SmartMart/backend/Supplier/controller/MongoConfig.java
@@ -12,7 +12,7 @@ public class MongoConfig {
 
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://<username>:<password>@cluster.mongodb.net/<database_name>?retryWrites=true&w=majority");
+        return MongoClients.create("mongodb+srv://<username>:123@cluster.mongodb.net/<database_name>?retryWrites=true&w=majority");
 
     }
 
