105c6667232586100b5d7336f83cf21dfc07d533
Indura <inpitadeniya@gmail.com>
2025-03-20 23:40:09 +0530

    create PurchaseOrder

---
+++ backend/src/main/java/com/SmartMart/backend/Supplier/model/PurchaseOrder.java
@@ -0,0 +1,20 @@
+package com.SmartMart.backend.Supplier.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document(collection = "purchase_orders")
+public class PurchaseOrder {
+    @Id
+    private String id;
+    private String supplierId;
+    private List<String> itemIds;
+    private String status; // e.g., "Pending", "Dispatched", "Delivered"
+    private LocalDateTime orderDate;
+    private LocalDateTime deliveryDate;
+}
